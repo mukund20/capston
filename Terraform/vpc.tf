f1c45f2be6f14a2bdc07e66e0c983e7a58b99c42
InquelTech <mukundatolay20@gmail.com>
2021-11-16 02:50:51 +0530

    test

---
+++ Terraform/vpc.tf
@@ -0,0 +1,37 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.78.0"
+
+  # VPC Basic Details
+  name            = "vpc-project"
+  cidr            = "10.0.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  # NAT Gateways - Outbound Communication
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  create_igw = true
+  
+  public_subnet_tags = {
+    Name = "Public-Subnet"
+    kubernetes.io/cluster/deveks = "shared"
+    kubernetes.io/role/elb = 1
+  }
+
+  private_subnet_tags = {
+    Name = "Public-Subnet"
+    kubernetes.io/cluster/deveks = "shared"
+    kubernetes.io/role/elb = 1
+  }
+
+  tags = {
+    Owner       = "mukund"
+    Environment = "project"
+  }
+
+  vpc_tags = {
+    Name = "vpc-project"
+  }
+}
