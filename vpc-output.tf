22b156e13c70541a60134834b1e5c013cb40586f
InquelTech <mukundatolay20@gmail.com>
2021-11-19 03:36:06 +0530

    added redis code

--- vpc-output.tf
+++ vpc-output.tf
@@ -1,11 +1,11 @@
-# VPC Output Values
+ VPC Output Values
 
 # VPC ID
 output "vpc_id" {
   description = "The ID of the VPC"
   value       = module.vpc.vpc_id
 }
-
+#
 # VPC CIDR blocks
 output "vpc_cidr_block" {
   description = "The CIDR block of the VPC"
